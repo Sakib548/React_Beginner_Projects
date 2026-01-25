ac22e850d2e0e8e67f519926d3cc553807491e1a
Md Sakib Hassan <Md Sakib Hassan>
2026-01-25 11:54:01 +0600

    meals

---
+++ meals/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./styles.css"
+import Meals from './components/Meals'
+
+const App = () => {
+  return (
+    <div>
+      <Meals/>
+    </div>
+  )
+}
+
+export default App
