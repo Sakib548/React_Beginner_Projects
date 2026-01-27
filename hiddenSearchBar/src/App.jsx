69ba6a9d67864ceb655148217e95495840798db8
Md Sakib Hassan <Md Sakib Hassan>
2026-01-27 11:03:50 +0600

    hiddenSearchBar

---
+++ hiddenSearchBar/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./styles.css"
+import HiddenSearchBar from './components/HiddenSearchBar'
+
+const App = () => {
+  return (
+    <div>
+      <HiddenSearchBar/>
+    </div>
+  )
+}
+
+export default App
